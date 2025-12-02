aa5be19ecfeb0b383c4c5ec2d44c3a7bba368997
AzeezAkinyemi999 <akinyemi.az@northeastern.edu>
2025-12-01 21:38:05 -0500

    translation about the binding atom

--- Calculations/CH2/solver.sh
+++ Calculations/CH2/solver.sh
@@ -5,5 +5,5 @@
 
 source ~/.bashrc
 conda deactivate
-conda activate mace_dev
+conda activate pynta_fairchem_dev 
 python /projects/westgroup/akinyemi.az/Hindered_Partition_Function_NEB/Calculations/NH3/solver.py
